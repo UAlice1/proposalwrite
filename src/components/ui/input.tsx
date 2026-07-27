5c42ca3b7cff0f17e70db0089c2ec9a7770b7fea
UAlice1 <umubyeyialice7@gmail.com>
2026-07-27 14:07:59 +0200

    changes

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     <input
       type={type}
       className={cn(
-        "flex h-9 w-full rounded-lg border-0 bg-background px-3 py-1 text-sm text-foreground shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:shadow-md focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50",
+        "flex h-9 w-full rounded-lg border-0 bg-background px-3 py-1 text-sm text-foreground shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:shadow-md focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 [&:-webkit-autofill]:shadow-[0_0_0px_1000px_white_inset] [&:-webkit-autofill]:[-webkit-text-fill-color:inherit]",
         className
       )}
       ref={ref}
